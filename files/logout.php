82942d7f7f63ee0d5fa507ee552f5979a471b37d
JEIVIVERACRUZ <jeivirodelas1@gmail.com>
2026-06-18 02:25:01 +0800

    Add files via upload

---
+++ files/logout.php
@@ -0,0 +1,8 @@
+<?php
+// ============================================================
+// logout.php  — Destroy session and redirect
+// ============================================================
+session_start();
+session_destroy();
+header('Location: index.php');
+exit;
